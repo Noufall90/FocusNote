3e3a55b8d11ce13d20b0f151c0dfa0f174b103cb
Noval ian <andrianau26@gmail.com>
2025-09-13 20:10:14 +0800

    note database

---
+++ focusnote_app/lib/Model/note_database.dart
@@ -0,0 +1,53 @@
+import 'package:focusnote_app/Model/note.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class NoteDatabase {
+  static late Isar isar;
+
+  // DATABASE
+  static Future<void> initialize() async {
+    final dir = await getApplicationCacheDirectory();
+    isar = await Isar.open(
+      [NoteSchema],
+      directory: dir.path,
+    );
+  }
+  
+  // LIST NOTE
+  final List<Note> currentNote = [];
+
+  // CREATE
+  Future<void> addNote(String text) async {
+    // new note object
+    final newNote = Note()..text = text; // pakai cascade operator
+
+    // save to db
+    await isar.writeTxn(() => isar.notes.put(newNote));
+    await readNote();
+  }
+
+  // READ
+  Future<void> readNote() async {
+    final readNotes = await isar.notes.where().findAll();
+    currentNote
+      ..clear()
+      ..addAll(readNotes);
+  }
+
+  // UPDATE
+  Future<void> updateNote(int id, String newText) async {
+    final existingNote = await isar.notes.get(id);
+    if (existingNote != null) {
+      existingNote.text = newText;
+      await isar.writeTxn(() => isar.notes.put(existingNote));
+      await readNote();
+    }
+  }
+
+  // DELETE
+  Future<void> deleteNote(int id) async {
+    await isar.writeTxn(() => isar.notes.delete(id));
+    await readNote();
+  }
+}
